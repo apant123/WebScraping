88332332485e6d3a312970af63589ad5fcccc298
Arav Pant <aravpant17@gmail.com>
2024-08-06 09:13:45 -0500

    Tweaked Google Fiber Scraper

--- Google2.py
+++ Google2.py
@@ -21,29 +21,49 @@ df = pd.read_csv('/Users/aravpant/Desktop/Projects/WebScraping/AddressList/Addre
 for index,row in df.iterrows():
   address = row['address_primary']
   zip = row['zip']
-  df['status'] = 'pending'
-
-  elem = WebDriverWait(driver, 5).until(
+  df.at[index, 'status'] = 'pending'
+  df.to_csv('/Users/aravpant/Desktop/Projects/WebScraping/AddressList/AddressWorking2.csv', index=False)
+  elem = WebDriverWait(driver, 2).until(
           EC.presence_of_element_located((By.CSS_SELECTOR, "input.address-checker__input.address-checker__input--street.borderable.pac-target-input"))
   )
   elem.clear()
   elem.send_keys(address)
 
-  zipcode = WebDriverWait(driver, 5).until(
+  zipcode = WebDriverWait(driver, 1).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="hero-carousal"]/div[2]/div/div[1]/form/div/div[1]/div[3]/input'))
   )
   zipcode.clear()
   zipcode.send_keys(zip)
   zipcode.send_keys(Keys.RETURN)
 
-  df['status'] = 'done'
+  try:
+    elements = WebDriverWait(driver, 1).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="mat-mdc-checkbox-2-input"]'))
+    )
+    df.at[index, 'status'] = 'Eligible'
+  except:
+    try:
+      elements1 = WebDriverWait(driver,1).until(
+        EC.presence_of_all_elements_located((By.XPATH,'/html/body/div[1]/address-app/div/cta-view/cta-container/cta-mailing/div/div/div[1]/div/h1' ))
+      )
+      df.at[index,'status'] = 'Unavailable'
+    except:
+      try:
+        element2 = WebDriverWait(driver,1).until(
+          EC.presence_of_all_elements_located((By.XPATH,'/html/body/div[1]/address-app/div/cta-view/cta-container/cta-already-registered/div/h1'))
+        )
+        df.at[index,'status'] = 'Has Account'
+      except:
+
+        df.at[index, 'status'] = 'Need Apt'
+
+
+  
+  df.to_csv('/Users/aravpant/Desktop/Projects/WebScraping/AddressList/AddressWorking2.csv', index=False)
+  
   driver.get("https://fiber.google.com/db/")
 
 
-
-
-
-
-time.sleep(10)
+time.sleep(1)
 df.to_csv('/Users/aravpant/Desktop/Projects/WebScraping/AddressList/AddressWorking2.csv', index=False)
 driver.quit()
